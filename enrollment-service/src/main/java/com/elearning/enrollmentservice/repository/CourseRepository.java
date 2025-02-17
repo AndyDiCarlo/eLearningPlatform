269f33be5a3bd82a9727d2329d15748773cd1387
nickBava <146784446+nickBava@users.noreply.github.com>
2025-02-16 19:17:00 -0500

    Added enrollment service

--- enrollment-service/src/main/java/com/elearning/enrollmentservice/repository/CourseRepository.java
+++ enrollment-service/src/main/java/com/elearning/enrollmentservice/repository/CourseRepository.java
@@ -4,5 +4,5 @@ import com.elearning.enrollmentservice.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
-    // Additional query methods as needed.
+
 }
--- enrollment-service/src/main/java/com/elearning/enrollmentservice/repository/UserRepository.java
+++ enrollment-service/src/main/java/com/elearning/enrollmentservice/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.elearning.enrollmentservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // Additional query methods as needed.
+
 }
